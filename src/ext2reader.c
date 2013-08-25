f19f4b4aea773d3197a474028307bb961d566f01
Azat Khuzhin <a3at.mail@gmail.com>
2013-08-26 00:40:45 +0400

    Pass DirIterate structure everywhere

--- src/ext2reader.c
+++ src/ext2reader.c
@@ -14,6 +14,9 @@
 
 struct DirIterate
 {
+    ext2_filsys fs;
+    ext2_inode_scan scanner;
+
     char prefix[PATH_MAX];
     char name[PATH_MAX];
 };
@@ -34,30 +37,23 @@ int dirIterator(ext2_ino_t dir,
     // TODO: do we need this?
     it->name[length] = '\0';
 
-    printf("%s\n", it->name);
+    printf("%s%s\n", it->prefix, it->name);
+
     return 0;
 }
 
-void readDirs(ext2_filsys fs)
+void readDirs(struct DirIterate *it)
 {
-    ext2_inode_scan scanner;
-    struct DirIterate it;
-
-    assert(!ext2fs_open_inode_scan(fs, 0 /* TODO: adjust */, &scanner));
-
     ext2_ino_t ino;
     struct ext2_inode inode;
-    while (!ext2fs_get_next_inode(scanner, &ino, &inode)) {
-        if (ext2fs_check_directory(fs, ino)) {
+    while (!ext2fs_get_next_inode(it->scanner, &ino, &inode)) {
+        if (ext2fs_check_directory(it->fs, ino)) {
             continue;
         }
 
         char buffer[PATH_MAX];
-        assert(!ext2fs_dir_iterate2(fs, ino, 0, buffer, dirIterator, &it));
+        assert(!ext2fs_dir_iterate2(it->fs, ino, 0, buffer, dirIterator, it));
     }
-
-
-    ext2fs_close_inode_scan(scanner);
 }
 
 
@@ -70,12 +66,17 @@ int main(int argc, char **argv)
 
     const char *dev = argv[1];
 
-    ext2_filsys fs;
-    assert(!ext2fs_open(dev, BLOCK_FLAG_READ_ONLY, 0, 0, unix_io_manager, &fs));
+    struct DirIterate it = {
+        .prefix = "/",
+        .name = ""
+    };
+    assert(!ext2fs_open(dev, BLOCK_FLAG_READ_ONLY, 0, 0, unix_io_manager, &it.fs));
+    assert(!ext2fs_open_inode_scan(it.fs, 0 /* TODO: adjust */, &it.scanner));
 
-    readDirs(fs);
+    readDirs(&it);
 
-    assert(!ext2fs_close(fs));
+    ext2fs_close_inode_scan(it.scanner);
+    assert(!ext2fs_close(it.fs));
 
     return EXIT_SUCCESS;
 }
